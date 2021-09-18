d311de1ddd74b73be90b36d9d718f59801bc8f07
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-09-18 17:18:20 -0500

    add Cartaview

---
+++ app/src/main/java/pe/edu/ulima/pm/ocholocos_asig1/view/CartaView.kt
@@ -0,0 +1,11 @@
+package pe.edu.ulima.pm.ocholocos_asig1.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class CartaView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+
+
+}
