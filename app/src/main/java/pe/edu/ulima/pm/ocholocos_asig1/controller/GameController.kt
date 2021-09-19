f85330fad88ffdff8088b7ac50be4687bf437b75
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-09-19 00:11:04 -0500

    fix barajas

--- app/src/main/java/pe/edu/ulima/pm/ocholocos_asig1/controller/GameController.kt
+++ app/src/main/java/pe/edu/ulima/pm/ocholocos_asig1/controller/GameController.kt
@@ -16,6 +16,9 @@ class GameController {
 fun main(args:Array<String>){
 
     var baraja = Baraja(52);
-
+    baraja.crearBaraja();
+    baraja.barajar();
+    baraja.getFirstCarta();
+    baraja.repartirCartas(8);
 
 }
--- app/src/main/java/pe/edu/ulima/pm/ocholocos_asig1/model/Baraja.kt
+++ app/src/main/java/pe/edu/ulima/pm/ocholocos_asig1/model/Baraja.kt
@@ -1,13 +1,13 @@
 package pe.edu.ulima.pm.ocholocos_asig1.model
 
 public class Baraja(val numCartas: Int) {
-    private var cartas = MutableList<Carta?>(numCartas) { null };
+    private var cartas = mutableListOf<Carta>();
     private val limitePalos: Int = 13;
     private val palos = arrayOf("Espadas", "Corazones", "Rombos", "Trebol");
 
     fun crearBaraja() {
-        for (i in 0..palos.size) {
-            for (j in 0..limitePalos) {
+        for (i in palos.indices) {
+            for (j in 0 until limitePalos) {
                 this.cartas.add(Carta(j + 1, palos[i]));
             }
         }
@@ -16,7 +16,7 @@ public class Baraja(val numCartas: Int) {
     fun barajar() {
         var posAleatorea: Int = 0;
         var c: Carta?;
-        for (i in 0..cartas.size) {
+        for (i in 0 until cartas.size) {
             posAleatorea = (0 until numCartas).random();
             c = cartas[i];
             cartas[i] = cartas[posAleatorea];
@@ -24,18 +24,18 @@ public class Baraja(val numCartas: Int) {
         }
     }
 
-    fun getFirstCarta(): Carta? {
-        var c: Carta? = cartas[0];
+    fun getFirstCarta(): Carta {
+        var c: Carta = cartas[0];
         cartas.removeAt(0)
         return c;
     }
 
-    fun repartirCartas(numCartas: Int): MutableList<Carta?>? {
+    fun repartirCartas(numCartas: Int): MutableList<Carta>? {
         if (numCartas > this.cartas.size) {
             // no se pueden repartir cartas
             return null;
         } else {
-            var mazo = MutableList<Carta?>(numCartas) { null };
+            var mazo = mutableListOf<Carta>();
             for (i in 0 until numCartas) {
                 mazo.add(this.getFirstCarta());
             }
